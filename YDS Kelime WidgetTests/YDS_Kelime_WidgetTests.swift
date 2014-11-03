45f0bee76cf05c9cf5804fc18652811cd21f494f
ayalcinkaya <ahmetyk@gmail.com>
2014-11-03 21:42:50 +0200

    Initial Commit

---
+++ YDS Kelime WidgetTests/YDS_Kelime_WidgetTests.swift
@@ -0,0 +1,36 @@
+//
+//  YDS_Kelime_WidgetTests.swift
+//  YDS Kelime WidgetTests
+//
+//  Created by Ahmet Yalcinkaya on 11/3/14.
+//  Copyright (c) 2014 Hayal Co. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class YDS_Kelime_WidgetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
